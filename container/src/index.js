15619f2ba8c701139318666b895f1dadf68fd99d
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-16 12:45:27 +0530

    created cart and integrated with container

--- container/src/index.js
+++ container/src/index.js
@@ -1,1 +1,1 @@
-console.log("container");
+import("./bootstrap"); // the reason why we import bootstrap instead of wrting the code here is, webpack can load all index.js together first, before loading bootstrap.js
